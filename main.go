a24dc65bb89b1d8294003b587a700418f288677e
Ilia <dudareviv@gmail.com>
2021-12-04 13:28:29 +0300

    Изменена концепция: теперь в основном файле будет калькулятор

--- main.go
+++ main.go
@@ -1,66 +1,43 @@
-package main
+package calculator
 
 import (
-	"os"
+	"errors"
+	"fmt"
 	"sort"
-	"strconv"
-	"strings"
 )
 
-func main() {
-	if len(os.Args) < 2 {
-		println("Необходимо указать сумму")
-		return
+type Calculator struct {
+	denominations []int
+}
+
+func NewCalculator(denominations []int) *Calculator {
+	c := new(Calculator)
+
+	c.denominations = denominations
+	sort.Ints(c.denominations)
+
+	return c
+}
+
+func (c *Calculator) Calculate(value int) (map[int]int, error) {
+	result := make(map[int]int)
+
+	reversedDenominations := reverse(c.denominations)
+
+	for _, denomination := range reversedDenominations {
+		var denominationCount = value / denomination
+		value -= denominationCount * denomination
+
+		result[denomination] = denominationCount
 	}
 
-	if len(os.Args) < 3 {
-		println("Необходимо указать номиналы и сумму")
-		return
+	if value > 0 {
+		message := fmt.Sprintf("Сумма не делится на цело. Остаток: %d", value)
+
+		return result, errors.New(message)
 	}
 
-	// Получить номиналы
-	var argumentNominals = strings.Split(os.Args[1], ",")
-	var nominals = make([]int, len(argumentNominals))
-
-	for i, nominal := range argumentNominals {
-		var convertedNominal, err = strconv.Atoi(nominal)
-
-		if err != nil {
-			println("Некорректный номинал", nominal, err)
-			return
-		}
-
-		nominals[i] = convertedNominal
-	}
-
-	sort.Ints(nominals)
-
-	// Получить сумму
-	var money, err = strconv.Atoi(os.Args[2])
-
-	if err != nil {
-		println("Некорректная сумма", os.Args[2], err)
-		return
-	}
-
-	// Проверить деление на минимальный номинал
-	var remainder = money % nominals[0]
-
-	if remainder > 0 {
-		println("Сумма не делится на самый минимальный номинал", nominals[0])
-		return
-	}
-
-	// Почитать и вывести результат
-	remainder = money
-	nominals = reverse(nominals)
-
-	for _, nominal := range nominals {
-		var nominalCount = remainder / nominal
-		remainder -= nominalCount * nominal
-
-		println("Номинал:", nominal, "Количество:", nominalCount)
-	}
+	return result, nil
 }
 
 func reverse(numbers []int) []int {
